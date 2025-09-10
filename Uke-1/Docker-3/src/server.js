f609aecd24617afea6b2aa95f5cfa99c605dbc0d
SanderNKL <sander.lastad@gmail.com>
2025-09-10 08:50:43 +0200

    added docker-3

---
+++ Uke-1/Docker-3/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000
+
+app.listen(port, () => {
+    console.log('Hello world')
+});
+
+app.get('/', (req, res, next) => {
+    res.status(200).send("Welcome to my website!")
+})
